7d24046e100f37c6b7afa8c9d05543237d876abb
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-02 02:05:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,1 @@
-class Dog
 
-   def name=(dogname)
-     @name = dogname
-   end
-   def name
-     @name
-   end
-   def breed=(breedname = "Mutt")
-     @breed = breedname
-   end
-   def breed
-     @breed
-   end
- end
